beb2b805f2ff9d3ce20a7dacc86a824ea422a03a
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2023-11-17 20:06:45 +0530

    Refactor App component and added counter component

--- pt1-state/src/App.jsx
+++ pt1-state/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./Button";
+import "./app.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="example-container">
+        <h1>State and Props</h1>
+        <p>Count: {count}</p>
+        <Button onClick={() => setCount(count + 1)}>Increment</Button>
+        <Button onClick={() => setCount(count - 1)}>Decrement</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
